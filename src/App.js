08ade6d9991a2d37f9e1e3cbba3cc761f5a0f0a9
sanjana-sharma29 <sanjana8117@gmail.com>
2026-01-15 16:54:51 +0530

    Fix logo SVG by moving to public folder

--- src/App.js
+++ src/App.js
@@ -11,7 +11,8 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+        href="https://www.propelleraero.com/dirtmate/"
+
           target="_blank"
           rel="noopener noreferrer"
         >
